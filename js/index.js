2f32e828203d286c29b9e6e804691c5fecf586b3
DanielMAssis <danielsenai123@gmail.com>
2021-02-04 00:25:35 -0300

    responsivo

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.fa-bars').click(function(){
+        $('#menu #mobile').slideToggle();
+    });
+    $('#menu #mobile li').click(function(){
+        $('#menu #mobile').slideToggle();
+    });
+});
